3d82d5d62593ae159fb9f1bbdc83ac88b31f3fc7
YabseraBogale <yabserapython@gmail.com>
2026-06-07 18:44:43 +0300

    adding gemini recommdection

--- src/algorthim.rs
+++ src/algorthim.rs
@@ -2,28 +2,28 @@ use std::f64;
 
 use chrono::{DateTime, Utc};
 pub struct TransactionCost{
-    commissions:f64,
-    number_of_shares:i64,
-    exchange_fee:f64,
-    taxes_fee:f64,
-    clearing_and_settlement_fees:f64,
-    slippage:f64,
+   pub commissions:f64,
+    pub number_of_shares:i64,
+    pub exchange_fee:f64,
+    pub taxes_fee:f64,
+    pub clearing_and_settlement_fees:f64,
+    pub slippage:f64,
 }
 pub struct Tick{
-    volume:f64,
-    price:f64,
+    pub volume:f64,
+    pub price:f64,
 }
 
 pub struct TimeWeightedAveragePrice{
-    share:f64,
-    price:f64,
+    pub share:f64,
+    pub price:f64,
 }
 
 pub struct Order{
-    tick:Tick,
-    side:String,
-    time_stamp:DateTime<Utc>,
-    order_type:String,
+    pub tick:Tick,
+    pub side:String,
+    pub time_stamp:DateTime<Utc>,
+    pub order_type:String,
 }
 
 pub fn percentage_of_volume(order_volume: f64, market_volume: f64)->f64 {
@@ -34,14 +34,17 @@ pub fn public_market_volume(participation_rate: f64, market_volume: f64)->f64 {
     return participation_rate * market_volume / (1.0 - participation_rate);
 }
 
-pub fn volume_weighted_averagePrice(t:&[Tick]) -> f64 {
+pub fn volume_weighted_average_price(t:&[Tick]) -> Option<f64> {
    let mut sum_volume=0.0;
    let mut sum: f64 = 0.0;
    for tick in t{
         sum_volume+=tick.volume;
         sum+=tick.price*tick.volume;
    }    
-   return sum/sum_volume;
+   if sum_volume==0.0{
+    return None;
+   }
+   return Some(sum/sum_volume);
 }
 
 pub fn bid_ask_spread(ask_price:f64,bid_price:f64)-> f64{
