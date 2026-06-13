a4a7fac9483d1782144a9345c15f80a36bdb7f73
YabseraBogale <yabserapython@gmail.com>
2026-06-13 20:30:57 +0300

    translated parically

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -25,7 +25,7 @@ func (v *VolumeWeightedAveragePriceTracker)new(){
         return VolumeWeightedAveragePriceTracker{ Cumulative_volume: 0.0, Cumulative_pv: 0.0 };
 }
 
-func (v *VolumeWeightedAveragePriceTracker) Update(price:float64,volume:float64)float64{
+func (v *VolumeWeightedAveragePriceTracker) Update(price float64,volume float64)float64{
         v.Cumulative_volume+=volume
         v.Cumulative_pv+=price*volume
         return v.Cumulative_pv/v.Cumulative_volume;
@@ -57,32 +57,34 @@ func (o *OrderBook)MidPrice()float64{
         return (best_bid+best_ask)/2.0
 }
 
-func (o *OrderBook)DelayCost(shares_executed:float64,price_arrival:float64,price_decision:float64)float64{
+func (o *OrderBook)DelayCost(shares_executed float64,price_arrival float64,price_decision float64)float64{
         return shares_executed*(price_arrival-price_decision)
-    }
+}
 
-func (o *OrderBook)TradingCost(shares_executed:float64,price_arrival:float64)float64{
+func (o *OrderBook)TradingCost(shares_executed float64,price_arrival float64)float64{
 	ok:=o.MidPrice()        	
     return shares_executed*(ok-price_arrival)
 
 }
 
-func (o *OrderBook)OpportunityCost(share_desired:float64,share_executed:float64,price_close:float64,price_decision:float64)float64{
+func (o *OrderBook)OpportunityCost(share_desired float64,share_executed float64,price_close float64,price_decision float64)float64{
         return (share_desired-share_executed)*(price_close-price_decision)
     }
 // to be implented
-func (o *OrderBook)ExplicitFees(t:TransactionCost)float64{
+func (o *OrderBook)ExplicitFees(t TransactionCost)float64{
+    
 }
 
-func PercentageOfVolume(order_volume: float64, market_volume: float64)float64 {
+
+func PercentageOfVolume(order_volume float64, market_volume float64)float64 {
     return order_volume / (order_volume + market_volume)
 }
 
-func PublicMarketVolume(participation_rate: float64, market_volume: float64)float64 {
+func PublicMarketVolume(participation_rate float64, market_volume float64)float64 {
     return participation_rate * market_volume / (1.0 - participation_rate)
 }
 
-func VolumeWeightedAveragePriceHistorical(t:[]Tick)float64{
+func VolumeWeightedAveragePriceHistorical(t []Tick)float64{
    sum_volume:=0.0
    sum:= 0.0
    for tick in t{
@@ -92,11 +94,11 @@ func VolumeWeightedAveragePriceHistorical(t:[]Tick)float64{
    return sum/sum_volume
 }
 
-func BidAskSpread(ask_price:float64,bid_price:float64)float64{
+func BidAskSpread(ask_price float64,bid_price float64)float64{
     return ask_price-bid_price
 }
 
-func ImplementationShortfAll(explicit_cost:float64,execution_cost:float64,slippage:float64,spread:float64,opportunity_cost:float64)float64{
+func ImplementationShortfAll(explicit_cost float64,execution_cost float64,slippage float64,spread float64,opportunity_cost float64)float64{
     return explicit_cost+(execution_cost*(slippage/spread))+opportunity_cost
 }
 
