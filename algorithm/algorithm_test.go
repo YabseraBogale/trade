7fcd3f6d344abb168699b93b6bb3123834b750f9
YabseraBogale <yabserapython@gmail.com>
2026-06-19 20:29:08 +0300

    marshaling

--- algorithm/algorithm_test.go
+++ algorithm/algorithm_test.go
@@ -1,25 +1,28 @@
 package algorithm
 
 import (
+	"crypto/rand"
 	"fmt"
 	"testing"
 	"trade/algorithm"
 )
 
 func TestFetchVolumeAndClosePriceFromURL(t *testing.T) {
-	url := "http://127.0.0.1:5000/"
-	res, err := algorithm.FetchVolumeAndClosePriceFromURL(url)
-	if err != nil {
-		t.Fatalf("Error: %v", err)
-	}
-	fmt.Println("Successfully processed: length of data ", len(res))
-}
 
-func TestFetchName(t *testing.T) {
 	url := "http://127.0.0.1:5000/name"
-	res, err := algorithm.FetchNameList(url)
+	resName, err := algorithm.FetchNameList(url)
+	if err != nil {
+		t.Fatalf("Failed Error: %v", err)
+	}
+
+	index, err := rand.Int(len(resName))
+	if err != nil {
+		t.Fatalf("Failed Error: %v", err)
+	}
+	url = "http://127.0.0.1:5000/" + resName[index]
+	resVolumeClosePrice, err := algorithm.FetchVolumeAndClosePriceFromURL(url)
 	if err != nil {
 		t.Fatalf("Error: %v", err)
 	}
-	fmt.Println("Successfully processed: length of data ", len(res))
+	fmt.Println("Successfully processed: length of data ", len(resVolumeClosePrice))
 }
